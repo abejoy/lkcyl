8497641a0f89b9e4971275dbae14f9696fd49d84
abejoy <abrahamjoys98@gmail.com>
2024-04-13 15:44:36 +0100

    Initial commit

---
+++ test/lkcyl.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Lkcyl from '../lib/lkcyl-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lkcyl-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Lkcyl.LkcylStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

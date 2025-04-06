653e282f2b0e83fad1f74448de31bbc4dc9ee4b2
abejoy <abrahamjoys98@gmail.com>
2025-04-06 21:08:14 +0100

    updated to prod

--- lib/lambdas/emailService/index.ts
+++ lib/lambdas/emailService/index.ts
@@ -1,14 +1,29 @@
-import { SESClient, SendEmailCommand, SendEmailCommandInput, SendRawEmailCommand, SendRawEmailCommandInput } from '@aws-sdk/client-ses'
-import { GetObjectCommand, GetObjectCommandOutput, S3Client  } from '@aws-sdk/client-s3'
-import { Color, Gender, Maybe, Player, Team } from '../helpers/types/graphql-types';
-import mime from 'mime-types';
-
+import {
+  SESClient,
+  SendEmailCommand,
+  SendEmailCommandInput,
+  SendRawEmailCommand,
+  SendRawEmailCommandInput,
+} from "@aws-sdk/client-ses";
+import {
+  GetObjectCommand,
+  GetObjectCommandOutput,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import {
+  Color,
+  Gender,
+  Maybe,
+  Player,
+  Team,
+} from "../helpers/types/graphql-types";
+import mime from "mime-types";
 
 export enum EmailTemplate {
-  Captian= 'Captian',
-  Manager = 'Manager',
-  Admin = 'Admin',
-  Custom = 'Custom',
+  Captian = "Captian",
+  Manager = "Manager",
+  Admin = "Admin",
+  Custom = "Custom",
 }
 
 export type EmailToSend = {
@@ -17,64 +32,84 @@ export type EmailToSend = {
   emailBody?: string;
   emailTemplate: EmailTemplate;
   emailArgs?: Team;
-}
+};
 
 type ReplacementMap = { [key: string]: string };
 type ReplacementArgs = {
-  name:string;
-  teamName:string;
-  unit:string;
-  gender:string;
-  allPlayers:string;
-  role:string;
-  color:string;
-  teamDetails:string;
-}
+  name: string;
+  teamName: string;
+  unit: string;
+  gender: string;
+  allPlayers: string;
+  role: string;
+  color: string;
+  teamDetails: string;
+};
 const emailBucketName = process.env.EMAIL_BUCKET_NAME;
-const Source: string = 'london.region.kcyl@gmail.com';
+const Source: string = "london.region.kcyl@gmail.com";
 
 const sesClient = new SESClient();
 const s3Clent = new S3Client();
 
-const sendEmail = async(ToAddresses: string[], subject: string = '', body:string = '') => {
+const sendEmail = async (
+  ToAddresses: string[],
+  subject: string = "",
+  body: string = ""
+) => {
   const params: SendEmailCommandInput = {
     Source,
-    Destination: {ToAddresses},
+    Destination: { ToAddresses },
     Message: {
-      Subject: {Data: subject},
-      Body: {Text: {Data: body}}
+      Subject: { Data: subject },
+      Body: { Text: { Data: body } },
     },
-  
   };
   return sesClient.send(new SendEmailCommand(params));
-}
+};
 
 // Helper function to convert stream to base64
 const streamToBase64 = (stream: NodeJS.ReadableStream): Promise<string> => {
   return new Promise((resolve, reject) => {
-      const chunks: Buffer[] = [];
-      stream.on('data', (chunk: Buffer) => chunks.push(chunk));
-      stream.on('error', reject);
-      stream.on('end', () => {
-          // Combine all chunks into a single buffer and then convert to Base64
-          const buffer = Buffer.concat(chunks);
-          resolve(buffer.toString('base64'));
-      });
+    const chunks: Buffer[] = [];
+    stream.on("data", (chunk: Buffer) => chunks.push(chunk));
+    stream.on("error", reject);
+    stream.on("end", () => {
+      // Combine all chunks into a single buffer and then convert to Base64
+      const buffer = Buffer.concat(chunks);
+      resolve(buffer.toString("base64"));
+    });
   });
-}
-const fetchS3Response = async(Key: string): Promise<GetObjectCommandOutput> =>{
+};
+
+const streamToString = (stream: NodeJS.ReadableStream): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const chunks: Buffer[] = [];
+    stream.on("data", (chunk) => chunks.push(chunk));
+    stream.on("error", reject);
+    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf-8")));
+  });
+};
+
+const fetchS3Response = async (
+  Key: string
+): Promise<GetObjectCommandOutput> => {
   const command = new GetObjectCommand({
     Bucket: emailBucketName,
-    Key
+    Key,
   });
   return s3Clent.send(command);
-}
-const fetchAndEncodeFile = async (Key: string): Promise<string| undefined> => {
+};
+const fetchAndEncodeFile = async (Key: string): Promise<string | undefined> => {
   const response = await fetchS3Response(Key);
   return streamToBase64(response.Body as NodeJS.ReadableStream); /// am i doing this right
-}
+};
 
-const sendEmailWithAttachment = async (recipients:string[], subject:string, bodyHtml: string, attachmentFillePathsInBucket: string[]) => {
+const sendEmailWithAttachment = async (
+  recipients: string[],
+  subject: string,
+  bodyHtml: string,
+  attachmentFillePathsInBucket: string[]
+) => {
   try {
     const recipientString = recipients.join(", ");
     const boundary = "----=_NextPart_000_001";
@@ -91,9 +126,10 @@ Content-Transfer-Encoding: 7bit
 ${bodyHtml}
 `;
 
-    for(const attachmentFilePath of attachmentFillePathsInBucket) {
+    for (const attachmentFilePath of attachmentFillePathsInBucket) {
       const attachmentContent = await fetchAndEncodeFile(attachmentFilePath);
-      const contentType = mime.lookup(attachmentFilePath) || "application/octet-stream";
+      const contentType =
+        mime.lookup(attachmentFilePath) || "application/octet-stream";
       const filename = attachmentFilePath.split("/").pop();
       rawMessage += `
 --${boundary}
@@ -105,98 +141,188 @@ ${attachmentContent}
 `;
     }
 
-
-
     const params: SendRawEmailCommandInput = {
-      RawMessage: {Data: Buffer.from(rawMessage)}
+      RawMessage: { Data: Buffer.from(rawMessage) },
     };
 
     const result = await sesClient.send(new SendRawEmailCommand(params));
-    return result
-  } catch(err) {
-    throw new Error(`Failed to send email with attachement ${JSON.stringify(err)}`);
+    return result;
+  } catch (err) {
+    throw new Error(
+      `Failed to send email with attachement ${JSON.stringify(err)}`
+    );
   }
-}
+};
 
-const replaceStringArgs = (template: string = '', replacements: ReplacementArgs): string => {
-  if(!template){
-    return '';
+const fetchAdminEmails = async (): Promise<string[]> => {
+  try {
+    // Fetch the file from S3
+    const response = await fetchS3Response("adminEmails.csv");
+
+    // Convert the response body (stream) to a string
+    const fileContent = await streamToString(
+      response.Body as NodeJS.ReadableStream
+    );
+
+    // Split the CSV content by commas and trim whitespace
+    const adminEmails = fileContent.split(",").map((email) => email.trim());
+    return adminEmails;
+  } catch (error) {
+    console.error("Error fetching or parsing the CSV file:", error);
+    throw error;
+  }
+};
+
+const replaceStringArgs = (
+  template: string = "",
+  replacements: ReplacementArgs
+): string => {
+  if (!template) {
+    return "";
   }
   let result = template;
   for (const key in replacements) {
-    const regex = new RegExp(`\\$${key}`, 'g');
+    const regex = new RegExp(`\\$${key}`, "g");
     result = result.replace(regex, replacements[key as keyof ReplacementArgs]);
   }
   return result;
-}
+};
 
-
-const getHtmlBody = async (s3TextPath: string, replacementParams:ReplacementArgs): Promise<string|undefined> => {
+const getHtmlBody = async (
+  s3TextPath: string,
+  replacementParams: ReplacementArgs
+): Promise<string | undefined> => {
   const response = await fetchS3Response(s3TextPath);
-  const htmlStringBeforeArgs =  await response.Body?.transformToString('utf-8');
+  const htmlStringBeforeArgs = await response.Body?.transformToString("utf-8");
   return replaceStringArgs(htmlStringBeforeArgs, replacementParams);
-}
+};
 
-const getAllPlayersAsFormatedString = (players: Maybe<Maybe<Player>[]> = []): string => {
-  const playersString: string[] = players?.map(player => player?.name || '') || [];
-  return playersString.join('<br/>');
-}
+const getAllPlayersAsFormatedString = (
+  players: Maybe<Maybe<Player>[]> = []
+): string => {
+  const playersString: string[] =
+    players?.map((player) => player?.name || "") || [];
+  return playersString.join("<br/>");
+};
 
-const getTeamDetailsText = (team:Team|string): string => {
-  if(typeof team === 'string') {
+const getTeamDetailsText = (team: Team | string): string => {
+  if (typeof team === "string") {
     return team;
   }
-  const players:string = getAllPlayersAsFormatedString(team.players);
+  const players: string = getAllPlayersAsFormatedString(team.players);
   return `<br/>Team Name: ${team.teamName}<br/><br/>Manager Details<br/>Name: ${team.managerName}<br/>Number:${team.managerPhone}<br/>Email:${team.managerEmail}<br/><br/>Captian Details:<br/>Name:${team.captianName}<br/>Number:${team.captainPhone}<br/>Email:${team.captianEmail}<br/><br/>Team Colour:${team.teamColor}<br/><br/>KcylUnit:${team.kcylUnit}<br/><br/>Aditional Message:${team.additionalMessage}<br/><br/>Players:<br/>${players}`;
-
-}
+};
 
 export const handler = async (event: any) => {
   try {
-    const toret:any = [];
+    const toret: any = [];
     const emailsToSend: EmailToSend[] = event.emailsToSend;
-    if(!Array.isArray(emailsToSend) || emailsToSend.length === 0) {
-      throw new Error('No emails to send');
+    if (!Array.isArray(emailsToSend) || emailsToSend.length === 0) {
+      throw new Error("No emails to send");
     }
-    let emailSentData: any;
-    for(const emailToSend of emailsToSend) {
-      const ToAddresses = emailToSend.emailAddressToSend;
-      const sub = 'National 7 aside Football Tournament'
-      const allPlayers: string = getAllPlayersAsFormatedString(emailToSend.emailArgs?.players);
-      const teamName = emailToSend.emailArgs?.teamName || '';
-      const unit = emailToSend.emailArgs?.kcylUnit || '';
-      const gender = emailToSend.emailArgs?.gender || '';
-      const color = emailToSend.emailArgs?.teamColor || '';
-      const teamDetails = getTeamDetailsText(emailToSend.emailArgs || 'undefined Team');
 
-      switch(emailToSend.emailTemplate) {
+    const adminEmailsToSend: string[] = await fetchAdminEmails();
+
+    let emailSentData: any;
+    for (const emailToSend of emailsToSend) {
+      const ToAddresses = emailToSend.emailAddressToSend;
+      const sub = "National 7 aside Football Tournament";
+      const allPlayers: string = getAllPlayersAsFormatedString(
+        emailToSend.emailArgs?.players
+      );
+      const teamName = emailToSend.emailArgs?.teamName || "";
+      const unit = emailToSend.emailArgs?.kcylUnit || "";
+      const gender = emailToSend.emailArgs?.gender || "";
+      const color = emailToSend.emailArgs?.teamColor || "";
+      const teamDetails = getTeamDetailsText(
+        emailToSend.emailArgs || "undefined Team"
+      );
+
+      switch (emailToSend.emailTemplate) {
         case EmailTemplate.Custom:
-          emailSentData = await sendEmail(ToAddresses, emailToSend.emailSubject, emailToSend.emailBody);
+          emailSentData = await sendEmail(
+            ToAddresses,
+            emailToSend.emailSubject,
+            emailToSend.emailBody
+          );
           break;
         case EmailTemplate.Captian:
-          const capReplacementParams: ReplacementArgs = { name: emailToSend.emailArgs?.captianName || '', teamName, unit, gender, allPlayers, role: emailToSend.emailTemplate, color, teamDetails};
-          const htmlBody: string = await getHtmlBody('emailbody/captianEmail.txt', capReplacementParams) || '';
-          emailSentData = await sendEmailWithAttachment(emailToSend.emailAddressToSend, sub, htmlBody, ['attachments/poster.jpeg']);
+          const capReplacementParams: ReplacementArgs = {
+            name: emailToSend.emailArgs?.captianName || "",
+            teamName,
+            unit,
+            gender,
+            allPlayers,
+            role: emailToSend.emailTemplate,
+            color,
+            teamDetails,
+          };
+          const htmlBody: string =
+            (await getHtmlBody(
+              "emailbody/captianEmail.txt",
+              capReplacementParams
+            )) || "";
+          emailSentData = await sendEmailWithAttachment(
+            emailToSend.emailAddressToSend,
+            sub,
+            htmlBody,
+            ["attachments/poster.jpeg"]
+          );
           break;
         case EmailTemplate.Manager:
-          const managerReplacementParams: ReplacementArgs = { name: emailToSend.emailArgs?.managerName || '', teamName, unit, gender, allPlayers, role: emailToSend.emailTemplate, color, teamDetails};
-          const managerHtmlBody: string = await getHtmlBody('emailbody/captianEmail.txt', managerReplacementParams) || '';
-          emailSentData = await sendEmailWithAttachment(emailToSend.emailAddressToSend, sub, managerHtmlBody, ['attachments/poster.jpeg']);
+          const managerReplacementParams: ReplacementArgs = {
+            name: emailToSend.emailArgs?.managerName || "",
+            teamName,
+            unit,
+            gender,
+            allPlayers,
+            role: emailToSend.emailTemplate,
+            color,
+            teamDetails,
+          };
+          const managerHtmlBody: string =
+            (await getHtmlBody(
+              "emailbody/captianEmail.txt",
+              managerReplacementParams
+            )) || "";
+          emailSentData = await sendEmailWithAttachment(
+            emailToSend.emailAddressToSend,
+            sub,
+            managerHtmlBody,
+            ["attachments/poster.jpeg"]
+          );
           break;
         case EmailTemplate.Admin:
-          const adminReplacementParams: ReplacementArgs = { name: emailToSend.emailArgs?.managerName || '', teamName, unit, gender, allPlayers, role: emailToSend.emailTemplate, color, teamDetails};
-          const adminHtmlBody:string = await getHtmlBody('emailbody/admin.txt', adminReplacementParams) || '';
-          emailSentData = await sendEmailWithAttachment(emailToSend.emailAddressToSend, `Team ${teamName} Registered`,adminHtmlBody ,[])
+          const adminReplacementParams: ReplacementArgs = {
+            name: emailToSend.emailArgs?.managerName || "",
+            teamName,
+            unit,
+            gender,
+            allPlayers,
+            role: emailToSend.emailTemplate,
+            color,
+            teamDetails,
+          };
+          const adminHtmlBody: string =
+            (await getHtmlBody(
+              "emailbody/admin.txt",
+              adminReplacementParams
+            )) || "";
+          emailSentData = await sendEmailWithAttachment(
+            adminEmailsToSend,
+            `Team ${teamName} Registered`,
+            adminHtmlBody,
+            []
+          );
           break;
         default:
           break;
       }
 
       toret.push(emailSentData);
-    };
+    }
     return { statusCode: 200, body: toret };
-
-  } catch(err) {
-    throw new Error(`Email failed to send ${err}`)
+  } catch (err) {
+    throw new Error(`Email failed to send ${err}`);
   }
-}
+};
--- lib/lambdas/teamService/index.ts
+++ lib/lambdas/teamService/index.ts
@@ -1,5 +1,9 @@
 import { DynamoDB, DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { InvokeCommand, InvokeCommandInput, LambdaClient } from "@aws-sdk/client-lambda";
+import {
+  InvokeCommand,
+  InvokeCommandInput,
+  LambdaClient,
+} from "@aws-sdk/client-lambda";
 import {
   DynamoDBDocumentClient,
   PutCommand,
@@ -10,14 +14,20 @@ import {
   ScanCommandInput,
 } from "@aws-sdk/lib-dynamodb";
 
-import { Team, AvailableColor, Color, Player, PlayerInput } from "../helpers/types/graphql-types";
+import {
+  Team,
+  AvailableColor,
+  Color,
+  Player,
+  PlayerInput,
+} from "../helpers/types/graphql-types";
 import { EmailTemplate, EmailToSend } from "../emailService";
 
 export type HttpResponse = {
-  statusCode: number,
-  headers: object,
-  body: string
-}
+  statusCode: number;
+  headers: object;
+  body: string;
+};
 
 interface ColorCounts {
   [key: string]: number;
@@ -27,84 +37,90 @@ interface TeamMutationInput extends Team {
   playerNames: string[];
 }
 
-
-const TableName = process.env.TABLE_NAME || '';
+const TableName = process.env.TABLE_NAME || "";
 
 const dynamoClient = new DynamoDBClient();
 const dynamo = DynamoDBDocumentClient.from(dynamoClient);
 
 const lambdaClinet = new LambdaClient();
 
-
-
 export const handler = async (event: any) => {
   switch (event.info.fieldName) {
-    case 'getTeam':
+    case "getTeam":
       return await getTeam(event.arguments.teamName);
-    case 'getAllTeam':
+    case "getAllTeam":
       return await getAllTeam();
-    case 'addTeam':
+    case "addTeam":
       return await addTeam(event.arguments);
-    case 'getAvailableColors':
+    case "getAvailableColors":
       return getTeamColors();
-    case 'getTableData':
+    case "getTableData":
       return getTableData();
-    case 'sendEmailsToCaptianManagers':
+    case "sendEmailsToCaptianManagers":
       return sendCustomEmail(event.arguments.subject, event.arguments.body);
-    case 'updateTeamPlayers':
-      return updateTeamPlayers(event.arguments.teamName, event.arguments.players);
+    case "updateTeamPlayers":
+      return updateTeamPlayers(
+        event.arguments.teamName,
+        event.arguments.players
+      );
     default:
       throw new Error("Handler not found");
-    break;
+      break;
   }
 };
 
-const updateTeamPlayers = async (teamName:string, players:PlayerInput[]): Promise<Team> => {
-  
+const updateTeamPlayers = async (
+  teamName: string,
+  players: PlayerInput[]
+): Promise<Team> => {
   try {
     const team = await getTeam(teamName);
-    if(!team) {
+    if (!team) {
       throw new Error(`Team not Found`);
     }
-    team.players = players
+    team.players = players;
     await updateTeam(team);
     return team as Team;
-  } catch(err) {
+  } catch (err) {
     throw new Error(`Failed to update team Players ${JSON.stringify(err)}`);
   }
-
-}
-
-
+};
 
 const getTableData = async (): Promise<Array<Array<string>>> => {
   const tableData: Array<Array<string>> = [];
   const scanParams: ScanCommandInput = {
     TableName,
-    ProjectionExpression: 'teamName, kcylUnit, teamColor, players'
-  }
+    ProjectionExpression: "teamName, kcylUnit, teamColor, players",
+  };
   let data;
   try {
     do {
       data = await dynamo.send(new ScanCommand(scanParams));
-      data.Items?.forEach(item => {
-        if(item.teamName){
-          const {teamName, kcylUnit, teamColor, players} = item
-          const rowTable: Array<string> = [teamName, kcylUnit, teamColor, areAllPlayersVerified(players)];
-          tableData.push(rowTable)
+      data.Items?.forEach((item) => {
+        if (item.teamName) {
+          const { teamName, kcylUnit, teamColor, players } = item;
+          const rowTable: Array<string> = [
+            teamName,
+            kcylUnit,
+            teamColor,
+            areAllPlayersVerified(players),
+          ];
+          tableData.push(rowTable);
         }
       });
-      scanParams.ExclusiveStartKey = data.LastEvaluatedKey
-    } while(data.LastEvaluatedKey);
+      scanParams.ExclusiveStartKey = data.LastEvaluatedKey;
+    } while (data.LastEvaluatedKey);
   } catch (err) {
-    throw new Error(`Failed to fetch Table data  DynamoDB ${JSON.stringify(err)}`);
+    throw new Error(
+      `Failed to fetch Table data  DynamoDB ${JSON.stringify(err)}`
+    );
   }
   return tableData;
-}
+};
 
 const areAllPlayersVerified = (players: Player[]): string => {
-  return String(players.every(player => (player.verified))).toUpperCase();
-}
+  return String(players.every((player) => player.verified)).toUpperCase();
+};
 
 const getAllTeam = async (): Promise<Team[]> => {
   const scanParams: ScanCommandInput = {
@@ -116,62 +132,71 @@ const getAllTeam = async (): Promise<Team[]> => {
     do {
       items = await dynamo.send(new ScanCommand(scanParams));
       items.Items?.forEach((item) => {
-        if(item.teamName){
+        if (item.teamName) {
           teamsScanned.push(item as Team);
         }
       });
-      scanParams.ExclusiveStartKey = items.LastEvaluatedKey
+      scanParams.ExclusiveStartKey = items.LastEvaluatedKey;
     } while (items.LastEvaluatedKey);
   } catch (err) {
-    throw new Error('Failed to fetch All team from DynamoDB');
+    throw new Error("Failed to fetch All team from DynamoDB");
   }
   return teamsScanned;
+};
 
-}
-
-
-const getTeam = async (teamName: string): Promise<Team| null> => {
+const getTeam = async (teamName: string): Promise<Team | null> => {
   const params: GetCommandInput = {
     TableName,
-    Key: { teamName }
+    Key: { teamName },
   };
   try {
-    const teamDynamoReturn = await dynamo.send(new GetCommand(params))
-    if(teamDynamoReturn.Item) {
+    const teamDynamoReturn = await dynamo.send(new GetCommand(params));
+    if (teamDynamoReturn.Item) {
       return teamDynamoReturn.Item as Team;
     }
     return null;
   } catch (err) {
     console.error(`Error getting team from team name ${teamName}`, err);
-    throw new Error('Failed to fetch team from DynamoDB');
+    throw new Error("Failed to fetch team from DynamoDB");
   }
-}
+};
 const sendEmail = async (emailsToSend: EmailToSend[]) => {
   const emailLambdaParams: InvokeCommandInput = {
     FunctionName: process.env.EMAIL_LAMBDA_NAME,
-    Payload: Buffer.from(JSON.stringify({emailsToSend})),
-  }
+    Payload: Buffer.from(JSON.stringify({ emailsToSend })),
+  };
   lambdaClinet.send(new InvokeCommand(emailLambdaParams));
   //for logging
   // const response = await lambdaClinet.send(new InvokeCommand(emailLambdaParams));
   // return new TextDecoder("utf-8").decode(response.Payload);
-}
+};
 
-const updateTeam = async (team:Team) => {
+const updateTeam = async (team: Team) => {
   const dynamoParams: PutCommandInput = {
     TableName,
     Item: team,
   };
   return dynamo.send(new PutCommand(dynamoParams));
-}
-
+};
 
 const addTeam = async (args: TeamMutationInput) => {
-  const {teamName, teamColor, managerEmail, managerName, captianName, captianEmail, kcylUnit, gender, additionalMessage, captainPhone, managerPhone} = args;
+  const {
+    teamName,
+    teamColor,
+    managerEmail,
+    managerName,
+    captianName,
+    captianEmail,
+    kcylUnit,
+    gender,
+    additionalMessage,
+    captainPhone,
+    managerPhone,
+  } = args;
   const isTeamNameTaken: boolean = await checkIfEntryExists(teamName);
   if (isTeamNameTaken) throw new Error(`Team Name: ${teamName} already exists`);
 
-  const players: Player[] = args.playerNames.map(playerName => ({
+  const players: Player[] = args.playerNames.map((playerName) => ({
     name: playerName,
     verified: false,
   }));
@@ -188,75 +213,89 @@ const addTeam = async (args: TeamMutationInput) => {
     gender,
     additionalMessage,
     captainPhone,
-    managerPhone
-  }
-
+    managerPhone,
+  };
 
   try {
-
     await updateTeam(teamToAdd);
 
-    const emailsToSendCaptian: EmailToSend = {emailAddressToSend: [teamToAdd.captianEmail], emailTemplate: EmailTemplate.Captian, emailArgs: teamToAdd};
+    const emailsToSendCaptian: EmailToSend = {
+      emailAddressToSend: [teamToAdd.captianEmail],
+      emailTemplate: EmailTemplate.Captian,
+      emailArgs: teamToAdd,
+    };
 
-    const emailsToSendManager: EmailToSend = {emailAddressToSend: [teamToAdd.managerEmail], emailTemplate: EmailTemplate.Manager, emailArgs: teamToAdd};
+    const emailsToSendManager: EmailToSend = {
+      emailAddressToSend: [teamToAdd.managerEmail],
+      emailTemplate: EmailTemplate.Manager,
+      emailArgs: teamToAdd,
+    };
+    const emailToAdmin: EmailToSend = {
+      emailAddressToSend: [],
+      emailTemplate: EmailTemplate.Admin,
+      emailArgs: teamToAdd,
+    };
 
-    const emailToAdmin: EmailToSend = {emailAddressToSend: ['kestertomy17@gmail.com', 'ivathomas2013@yahoo.com', 'abrahamjoys98@outlook.com'], emailTemplate: EmailTemplate.Admin, emailArgs: teamToAdd};
-
-    const emailsToSend: EmailToSend[] = [emailsToSendCaptian, emailsToSendManager, emailToAdmin]
+    const emailsToSend: EmailToSend[] = [
+      emailsToSendCaptian,
+      emailsToSendManager,
+      emailToAdmin,
+    ];
     sendEmail(emailsToSend);
     return teamToAdd;
   } catch (err) {
     throw new Error(`Failed to addteam ${JSON.stringify(err)}`);
   }
-}
+};
 
-const sendCustomEmail = (subject:string, body:string): string => {
-  return 'todo';
-}
+const sendCustomEmail = (subject: string, body: string): string => {
+  return "todo";
+};
 
 const checkIfEntryExists = async (teamName: string): Promise<boolean> => {
   const params: GetCommandInput = {
-      TableName,
-      Key: {teamName},
-      ProjectionExpression: "teamName"  // Specify a small attribute or one that exists in all records
+    TableName,
+    Key: { teamName },
+    ProjectionExpression: "teamName", // Specify a small attribute or one that exists in all records
   };
   try {
-      const data = await dynamo.send(new GetCommand(params));
-      if (data.Item) {
-          return true;
-      } else {
-          return false;
-      }
+    const data = await dynamo.send(new GetCommand(params));
+    if (data.Item) {
+      return true;
+    } else {
+      return false;
+    }
   } catch (err) {
     throw new Error(`Failed to check if entry exists ${JSON.stringify(err)}`);
   }
-}
+};
 
 //todo
-const getTeamColors = async(): Promise<Array<AvailableColor>> => {
+const getTeamColors = async (): Promise<Array<AvailableColor>> => {
   const colorCounts: ColorCounts = {};
   const scanParams: ScanCommandInput = {
     TableName,
-    ProjectionExpression: "teamColor"
+    ProjectionExpression: "teamColor",
   };
   try {
     let scanResults;
     do {
       scanResults = await dynamo.send(new ScanCommand(scanParams));
-      scanResults.Items?.forEach(item => {
-        if(item.teamColor) {
-          colorCounts[item.teamColor] = (colorCounts[item.teamColor] || 0) + 1
+      scanResults.Items?.forEach((item) => {
+        if (item.teamColor) {
+          colorCounts[item.teamColor] = (colorCounts[item.teamColor] || 0) + 1;
         }
       });
       scanParams.ExclusiveStartKey = scanResults.LastEvaluatedKey;
-    } while (scanResults.LastEvaluatedKey)
-    const availableColors: AvailableColor[] = Object.values(Color).map(color => ({
-      colorName: color,
-      available: (colorCounts[color] || 0) < 2
-    }));
+    } while (scanResults.LastEvaluatedKey);
+    const availableColors: AvailableColor[] = Object.values(Color).map(
+      (color) => ({
+        colorName: color,
+        available: (colorCounts[color] || 0) < 2,
+      })
+    );
     return availableColors;
-
   } catch (err) {
-    throw new Error('Failed to fetch All team colors from DynamoDB');
+    throw new Error("Failed to fetch All team colors from DynamoDB");
   }
-}
+};
